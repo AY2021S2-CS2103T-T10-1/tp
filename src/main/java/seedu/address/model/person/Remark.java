c5fd625b9bd9ad71e82b0cb669c634391b3ef6fe
Wei Yutong <weiyutongggg@gmail.com>
2021-03-01 12:11:42 +0800

    Edit to fit checkstyle

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,10 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructor method for remark
+     * @param remark takes in a String remark
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
